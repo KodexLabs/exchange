36d22e33ebc3782eb1115d1c2d539f4165389c80
p7m <perpetuum7@protonmail.com>
2022-11-19 11:05:16 +0100

    update imports for oz

--- src/KodexExchange.sol
+++ src/KodexExchange.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.11;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {EIP712} from "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {EIP712} from "openzeppelin-contracts/utils/cryptography/draft-EIP712.sol";
+import {Pausable} from "openzeppelin-contracts/security/Pausable.sol";
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {ERC721} from "solmate/tokens/ERC721.sol";
 
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
-import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+import {ECDSA} from "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
+import {Counters} from "openzeppelin-contracts/utils/Counters.sol";
 
 /**
  * @title KodexExchange
