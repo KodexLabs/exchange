e4b2da066667ac021fbf1fbbc99697aa6c960d1a
p7m <perpetuum7@protonmail.com>
2022-11-19 18:59:24 +0100

    init a mock weth token

--- test/utils/KodexExchangeTest.sol
+++ test/utils/KodexExchangeTest.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.17;
 
 import {Test, stdStorage, StdStorage} from "forge-std/Test.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 
 import {KodexExchange} from "../../src/KodexExchange.sol";
 
 abstract contract KodexExchangeTest is Test {
     using stdStorage for StdStorage;
 
+    MockERC20 internal weth;
+
     KodexExchange internal exchange;
+
+    function setUp() public virtual {
+        weth = new MockERC20("Wrapped Ether", "WETH", 18);
+    }
 }
